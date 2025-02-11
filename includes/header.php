fb4c55c7adafc232bd123f0cd05859efb75bee78
[kpatel2-23] <[cokp2@lunet.lboro.ac.uk]>
2025-02-11 17:00:29 +0000

    common header

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<!-- header.php -->
+<header>
+  <h1>My Company</h1>
+  <nav>
+    <a href="index.php">Home</a>
+    <a href="project.php">Project</a>
+    <a href="kanban.php">Kanban</a>
+  </nav>
+</header>
+<hr>
